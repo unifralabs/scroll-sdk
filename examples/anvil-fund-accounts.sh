e3ab50f02e09971a326c056a43b97884ba38bbba
Tony <tony@unifra.io>
2025-04-09 21:50:04 -0400

    Update anvil-fund-accounts.sh

--- examples/anvil-fund-accounts.sh
+++ examples/anvil-fund-accounts.sh
@@ -5,14 +5,15 @@ read_config() {
     yq eval "$1" charts/scroll-sdk/config.toml
 }
 
-L1_RPC_URL="l1-2.devnet.scroll.tech"
+L1_RPC_URL="l1-devnet.scroll.tech"
 
 # Array of addresses
 addresses=(
   "$(read_config '.accounts.L1_COMMIT_SENDER_ADDR')"
   "$(read_config '.accounts.L1_FINALIZE_SENDER_ADDR')"
   "$(read_config '.accounts.L1_GAS_ORACLE_SENDER_ADDR')"
-  "0x98110937b5D6C5FCB0BA99480e585D2364e9809C"
+  "$(read_config '.accounts.DEPLOYER_ADDR')"
+  "$(read_config '.accounts.OWNER_ADDR')"
 )
 
 # Loop through each address and call the curl command
